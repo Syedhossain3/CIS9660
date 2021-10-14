ad65b4e6c1a096c6d8932987d806701fc17b13a8
Syed Hossain <syed.hossain3@baruchmail.cuny.edu>
2021-10-14 00:40:02 -0400

    CIS9660 Midterm

--- Midterm.py
+++ Midterm.py
@@ -2,5 +2,5 @@ import pandas as pd
 #import mathplot as plt
 
 stock = pd.read_csv('C:\Ddrive\OneDrive - The City University of New York\Baruch College\Zickline Business School\MBA\Fall 2021\CIS 9660\Sample Data\stock_prices_2021.csv')
-print(stock)
+print('something')
 
